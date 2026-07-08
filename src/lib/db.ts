7e643df6348378e48ed4e6ee2536183f813f0ad2
calinma <calin.marincas@yahoo.com>
2026-07-08 12:39:19 +0300

    Fix database connection SSL setting

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,6 +5,7 @@ const pool = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  ssl: { rejectUnauthorized: false },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
